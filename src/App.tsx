4d21f5ada9a14cfd2ec91dfc4f3d825edab49583
shinobiseb <sebpatin@gmail.com>
2023-08-28 22:48:28 -0400

    initial deploy

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Nav from "./components/Nav"
+import MainContainer from "./components/MainContainer"
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+      <MainContainer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainContainer() {
+  return (
+    <div>
+        <h1 className='text-3xl'></h1>
+    </div>
+  )
+}
