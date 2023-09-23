c687d764f54ccc87117671700f8beb7511456c0a
shinobiseb <sebpatin@gmail.com>
2023-09-23 18:11:26 -0400

    form2 created

---
+++ src/components/Form2.tsx
@@ -0,0 +1,8 @@
+
+export default function Form2() {
+  return (
+    <div className='shadow-lg min-w-[768px] w-2/5 h-4/5 bg-white z-10 border-solid rounded-md p-10 flex flex-col items-center justify-evenly '>
+      <input className='button' type="checkbox" name="" id="" />
+    </div>
+  )
+}
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Form from './Form';
 import Welcome from './Welcome';
+import Form2 from './Form2';
 
 export default function MainContainer() {
   return (
     <div className="hero-bg relative flex items-center justify-center h-screen w-screen overflow-hidden">
-      <Form/>
+      <Form2/>
     </div>
   );
 }
